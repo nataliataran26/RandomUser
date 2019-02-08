d819426170a9f51b8654059045f1443c60b00065
Natalia Taran <nataliataran26@gmail.com>
2019-02-08 18:17:01 +0200

    Develop the first screen with a list of Users.

--- app/src/main/java/com/nataliiataran/randomuser/MainActivity.java
+++ app/src/main/java/com/nataliiataran/randomuser/MainActivity.java
@@ -2,12 +2,51 @@ package com.nataliiataran.randomuser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.nataliiataran.randomuser.api.ApiResponse;
+import com.nataliiataran.randomuser.api.Result;
+import com.nataliiataran.randomuser.api.UserAdapter;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvUserList;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvUserList = findViewById(R.id.rvUserList);
+        progressBar = findViewById(R.id.progressBar);
+
+        final UserAdapter adapter = new UserAdapter();
+        rvUserList.setLayoutManager(new LinearLayoutManager(this));
+        rvUserList.setAdapter(adapter);
+
+        RetrofitInterface client = RetrofitClient.getClient().create(RetrofitInterface.class);
+        client.fetchUsers(10).enqueue(new Callback<ApiResponse>() {
+            @Override
+            public void onResponse(Call<ApiResponse> call, Response<ApiResponse> response) {
+                List<Result> mlist = response.body().getResults();
+                progressBar.setVisibility(View.INVISIBLE);
+                adapter.setData(mlist);
+            }
+
+            @Override
+            public void onFailure(Call<ApiResponse> call, Throwable t) {
+                Log.e("Main", t.toString());
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/nataliiataran/randomuser/api/UserAdapter.java
@@ -0,0 +1,65 @@
+package com.nataliiataran.randomuser.api;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.nataliiataran.randomuser.R;
+
+import java.util.Collections;
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder> {
+
+    private List<Result> mlist = Collections.emptyList();
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_card, parent, false);
+        return new UserViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+        holder.bind(mlist.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mlist.size();
+    }
+
+    public void setData(List<Result> mlist) {
+        this.mlist = mlist;
+        notifyDataSetChanged();
+    }
+
+    class UserViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView ivAvatar;
+        private TextView tvName;
+        private TextView tvLastName;
+        private TextView tvCity;
+
+        public UserViewHolder(View itemView) {
+            super(itemView);
+            ivAvatar = itemView.findViewById(R.id.ivAvatar);
+            tvName = itemView.findViewById(R.id.tvFirstName);
+            tvLastName = itemView.findViewById(R.id.tvLastName);
+            tvCity = itemView.findViewById(R.id.tvCity);
+        }
+
+        public void bind(Result result) {
+            tvName.setText(result.getName().getFirst());
+            tvLastName.setText(result.getName().getLast());
+            tvCity.setText(result.getLocation().getCity());
+            Glide.with(ivAvatar).load(result.getPicture().getMedium()).into(ivAvatar);
+        }
+    }
+}
