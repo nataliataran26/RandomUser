5fac81dacf1a28f10a7feca326a812babac1c4ec
Natalia Taran <nataliataran26@gmail.com>
2019-02-08 18:44:41 +0200

    Develop the screen with a description of the user.

--- app/src/main/java/com/nataliiataran/randomuser/MainActivity.java
+++ app/src/main/java/com/nataliiataran/randomuser/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(R.string.home);
 
         rvUserList = findViewById(R.id.rvUserList);
         progressBar = findViewById(R.id.progressBar);
